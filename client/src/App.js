bc448e30b1f866387d0ffbd5b79b15595e6b5ef7
Cal Richards <calrichards@me.com>
2021-02-17 18:56:18 +0000

    basic setup file structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+
+import 'bulma'
+import './styles/style.scss'
 
 const App = () => (
   <h1>Hello world</h1>
